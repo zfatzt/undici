1ef2d1dc6a3692e0393176b950dd8c73266b6f9d
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-02-10 11:22:08 +0100

    Datenbank kann von jedem Computer im BBC aufgerufen werden.

--- src/undici/undici/ConnectionFactory.java
+++ src/undici/undici/ConnectionFactory.java
@@ -12,7 +12,7 @@ public class ConnectionFactory {
 		private String dbPwd;
 		
 		private ConnectionFactory() {
-			setDbURL("jdbc:mysql://localhost:3306/?useSSL=false");
+			setDbURL("jdbc:mysql://192.168.3.183:3306/?useSSL=false");
 			setDbUser("root");
 			setDbPwd("1234");
 		}
