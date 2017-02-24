5abc2cdb9230c767e3d459f80ba6914d5a850454
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-02-24 13:22:16 +0100

    Imports überarbeitet

--- src/undici/GUI/PizzaFrame.java
+++ src/undici/GUI/PizzaFrame.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
--- src/undici/GUI/ProductBox.java
+++ src/undici/GUI/ProductBox.java
@@ -2,7 +2,6 @@ package undici.GUI;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
